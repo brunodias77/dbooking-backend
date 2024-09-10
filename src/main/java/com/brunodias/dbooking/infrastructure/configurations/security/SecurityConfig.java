f4b18780537c3206111b969c81338d4eb8ec55f7
brunodias77 <brunohdias95@gmail.com>
2024-09-10 16:17:41 -0300

    Refactor SecurityConfig to add CORS configuration

--- src/main/java/com/brunodias/dbooking/infrastructure/configurations/security/SecurityConfig.java
+++ src/main/java/com/brunodias/dbooking/infrastructure/configurations/security/SecurityConfig.java
@@ -17,6 +17,11 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 @RequiredArgsConstructor
@@ -50,18 +55,30 @@ public class SecurityConfig {
     }
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf(AbstractHttpConfigurer::disable)
-                .exceptionHandling(
-                        exception -> exception.authenticationEntryPoint(_jwtAuthEntryPoint))
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/admin/**").hasRole("ADMIN") // Permitir apenas usuários com ROLE_ADMIN na rota
-                        .anyRequest().permitAll());
-        http.authenticationProvider(authenticationProvider());
-        http.addFilterBefore(authenticationTokenFilter(), UsernamePasswordAuthenticationFilter.class);
-        return http.build();
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("http://localhost:3000")); // Permitir chamadas de localhost:3000
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS")); // Métodos permitidos
+        configuration.setAllowedHeaders(List.of("*")); // Permitir todos os cabeçalhos
+        configuration.setAllowCredentials(true); // Permitir envio de credenciais (cookies, headers de autenticação)
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
-
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.cors().and() 
+                .csrf(AbstractHttpConfigurer::disable) 
+                .exceptionHandling(
+                        exception -> exception.authenticationEntryPoint(_jwtAuthEntryPoint)) // Configurar EntryPoint para lidar com exceções
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // Definir a política de sessão como STATELESS
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/admin/**").hasRole("ADMIN") // Somente ADMIN pode acessar /admin/**
+                        .requestMatchers("/user/**").hasAnyRole("USER", "ADMIN") // Somente USER ou ADMIN podem acessar /user/**
+                        .anyRequest().permitAll()); // Qualquer outra requisição é permitida
+        http.authenticationProvider(authenticationProvider()); // Configurar o provedor de autenticação
+        http.addFilterBefore(authenticationTokenFilter(), UsernamePasswordAuthenticationFilter.class); // Adicionar o filtro de autenticação antes do UsernamePasswordAuthenticationFilter
+        return http.build();
+    }
 }
